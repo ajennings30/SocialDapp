670983fc4cd3c279eca36eda2f84d6e3f140bb73
Al Jennings <aledjennings@gmail.com>
2020-07-08 05:22:29 -0400

    Updates

---
+++ precache-manifest.c3be055d282c8085debb0fbc1d94eb1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "18b4336f0151974cb65a",
+    "url": "/SocialDapp/static/js/runtime~main.18b4336f.js"
+  },
+  {
+    "revision": "48cb34a496cd3339489a",
+    "url": "/SocialDapp/static/js/main.48cb34a4.chunk.js"
+  },
+  {
+    "revision": "a0548da73fb3e8a623d2",
+    "url": "/SocialDapp/static/js/1.a0548da7.chunk.js"
+  },
+  {
+    "revision": "48cb34a496cd3339489a",
+    "url": "/SocialDapp/static/css/main.d7fbbb7a.chunk.css"
+  },
+  {
+    "revision": "a0548da73fb3e8a623d2",
+    "url": "/SocialDapp/static/css/1.e5866a1a.chunk.css"
+  },
+  {
+    "revision": "3a5e2129145e47d66b25f18bb2578702",
+    "url": "/SocialDapp/index.html"
+  }
+];
